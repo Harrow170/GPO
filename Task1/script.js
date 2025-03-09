ce3eab0dbd56cc75747cabf65517528bd46a0997
Harrow170 <krakfilipp@gmail.com>
2025-03-09 12:00:03 +0300

    <First try>

---
+++ Task1/script.js
@@ -0,0 +1,9 @@
+document.getElementById('class_component_button').addEventListener('click', function()
+{
+    window.location.href = 'class_component.html'
+});
+
+document.getElementById('functional_component_button').addEventListener('click', function()
+{
+    window.location.href = 'functional_component.html'
+});
